945f5ec66116bc2a29d500745b038ce6ea9b1017
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2024-12-07 04:03:36 +0000

    Remove unneeded variables

--- Event.lua
+++ Event.lua
@@ -1,7 +1,5 @@
 --!strict
-local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local RunService = game:GetService("RunService")
 
 local Types = require(script.Parent.Types)
 
