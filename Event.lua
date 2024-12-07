ca604ba659bd103d4953d19bff0481c2d201c7f4
Cyrus <63274991+cyrus01337@users.noreply.github.com>
2024-12-07 03:45:07 +0000

    Add Event

---
+++ Event.lua
@@ -0,0 +1,127 @@
+--!strict
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+local Types = require(script.Parent.Types)
+
+local BasicEventWrapper = {}
+local Event = {}
+
+type BasicEventWrapperType<Arguments...> = {
+	Original: RBXScriptSignal,
+	
+	connect: (Types.Callback<Arguments...>) -> (),
+	once: (Types.Callback<Arguments...>) -> (),
+	wait: () -> Arguments...,
+}
+
+type BaseEvent = {
+	Name: string,
+}
+type ClientSideEvent<Arguments...> = BaseEvent & {
+	OnClientEvent: BasicEventWrapperType<Arguments...>,
+
+	fireClient: Types.Callback<(Player, Arguments...)>,
+	fireAllClients: Types.Callback<Arguments...>,
+}
+type ServerSideEvent<Arguments...> = BaseEvent & {
+	OnServerEvent: BasicEventWrapperType<(Player, Arguments...)>,
+
+	fireServer: Types.Callback<Arguments...>,
+}
+type BidirectionalEvent<Arguments...> = ClientSideEvent<Arguments...> & ServerSideEvent<Arguments...>
+
+export type ClientSide<Arguments... = ()> = ClientSideEvent<Arguments...>
+export type ServerSide<Arguments... = ()> = ServerSideEvent<Arguments...>
+export type Bidirectional<Arguments... = ()> = BidirectionalEvent<Arguments...>
+export type Any<Arguments... = ()> = ClientSideEvent<Arguments...> |
+	ServerSideEvent<Arguments...> |
+	BidirectionalEvent<Arguments...>
+
+function BasicEventWrapper.new<Arguments...>(event: RBXScriptSignal): BasicEventWrapperType<Arguments...>
+	local self = {
+		Original = event,
+	}
+	
+	function self.connect(callback)
+		self.Original:Connect(callback)
+	end
+	
+	function self.once(callback)
+		self.Original:Once(callback)
+	end
+	
+	function self.wait()
+		return self.Original:Wait()
+	end
+	
+	return self
+end
+
+function findRelatedRemoteEvent(name: string): RemoteEvent
+	local remoteEventFound: RemoteEvent? = ReplicatedStorage:FindFirstChild(name)
+
+	if not remoteEventFound then
+		error(`Could not find remote event "{name}"`)
+	end
+
+	return remoteEventFound
+end
+
+function Event.clientSide<Arguments...>(name: string): ClientSideEvent<Arguments...>
+	local remoteEvent = findRelatedRemoteEvent(name)
+	local self = {
+		Name = name,
+		OnClientEvent = BasicEventWrapper.new(remoteEvent.OnClientEvent),
+	}
+	
+	function self.fireClient(player, ...)
+		remoteEvent:FireClient(player, ...)
+	end
+
+	function self.fireAllClients(...)
+		remoteEvent:FireAllClients(...)
+	end
+
+	return self
+end
+
+function Event.serverSide<Arguments...>(name: string): ServerSideEvent<Arguments...>
+	local remoteEvent = findRelatedRemoteEvent(name)
+	local self = {
+		Name = name,
+		OnServerEvent = BasicEventWrapper.new(remoteEvent.OnServerEvent),
+	}
+
+	function self.fireServer(...)
+		remoteEvent:FireServer(...)
+	end
+	
+	return self
+end
+
+function Event.bidirectional<Arguments...>(name: string): BidirectionalEvent<Arguments...>
+	local remoteEvent = findRelatedRemoteEvent(name)
+	local self = {
+		Name = name,
+		OnClientEvent = BasicEventWrapper.new(remoteEvent.OnClientEvent),
+		OnServerEvent = BasicEventWrapper.new(remoteEvent.OnServerEvent),
+	}
+	
+	function self.fireServer(...)
+		remoteEvent:FireServer(...)
+	end
+	
+	function self.fireClient(player, ...)
+		remoteEvent:FireClient(player, ...)
+	end
+
+	function self.fireAllClients(...)
+		remoteEvent:FireAllClients(...)
+	end
+	
+	return self
+end
+
+return Event
